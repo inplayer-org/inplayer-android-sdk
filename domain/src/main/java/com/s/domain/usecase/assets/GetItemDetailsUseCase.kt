1d82a0b008a1301e608147b47e60f45d07111705
Viktor <viktor.petrovski@adeva.co>
2019-07-09 08:31:58 +0200

    Adjusted GetItemDetailsUseCase

--- domain/src/main/java/com/s/domain/usecase/assets/GetItemDetailsUseCase.kt
+++ domain/src/main/java/com/s/domain/usecase/assets/GetItemDetailsUseCase.kt
@@ -11,16 +11,22 @@ import io.reactivex.Single
  */
 class GetItemDetailsUseCase(mySchedulers: MySchedulers, val inPlayerAssetsRepository: InPlayerAssetsRepository)
     : SingleUseCase<ItemDetailsEntity, GetItemDetailsUseCase.Params>(mySchedulers) {
-    
+
     override fun buildUseCaseObservable(params: Params?): Single<ItemDetailsEntity> {
-        
+
         params?.let {
-            return inPlayerAssetsRepository.getItemDetails(it.id, it.merchantUUID)
+            return when(params){
+                is Params.ItemDetailsParams -> inPlayerAssetsRepository.getItemDetails(params.id, params.merchantUUID)
+                is Params.ExternalAssetParams -> inPlayerAssetsRepository.getExternalAsset(params.assetType, params.externalId)
+            }
         }
-        
+
         throw IllegalStateException("Params can't be null for GetItemDetailsUseCase")
-        
+
     }
-    
-    data class Params(val id: Int, val merchantUUID: String)
+
+    sealed class Params {
+        data class ItemDetailsParams(val id: Int, val merchantUUID: String) : Params()
+        data class ExternalAssetParams(val assetType: String, val externalId: String) : Params()
+    }
 }
