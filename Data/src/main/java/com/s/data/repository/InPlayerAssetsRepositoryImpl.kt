a987d72d06ed1bdc3c145285fff11052bef5c845
Viktor <viktor.petrovski@adeva.co>
2019-07-09 08:24:56 +0200

    Implemented getExternalAsset in Repository

--- Data/src/main/java/com/s/data/repository/InPlayerAssetsRepositoryImpl.kt
+++ Data/src/main/java/com/s/data/repository/InPlayerAssetsRepositoryImpl.kt
@@ -17,22 +17,28 @@ class InPlayerAssetsRepositoryImpl(private val assetsRemote: AssetsRemote,
                                    private val mapAccessFee: MapDataAccessFee,
                                    private val mapItemDetails: MapDataItemDetails,
                                    private val mapItemAccess: MapDataItemAccess) : InPlayerAssetsRepository {
-    
+
+
     override fun getItemDetails(id: Int, merchantUUID: String): Single<ItemDetailsEntity> {
         return assetsRemote.getItemDetails(id = id, merchantUUID = merchantUUID).map {
             mapItemDetails.mapFromModel(it)
         }
     }
-    
+
     override fun getItemAccess(id: Int): Single<ItemAccessEntity> {
         return assetsRemote.getItemAccess(id = id)
                 .map { mapItemAccess.mapFromModel(it) }
     }
-    
+
+    override fun getExternalAsset(assetType: String, externalId: String): Single<ItemDetailsEntity> {
+        return assetsRemote.getExternalAsset(assetType = assetType, externalId = externalId)
+                .map { mapItemDetails.mapFromModel(it) }
+    }
+
     override fun getAccessFees(id: Int): Single<List<AccessFeeEntity>> {
         return assetsRemote.getAccessFees(id).map { list ->
             list.map { mapAccessFee.mapFromModel(it) }
         }
     }
-    
+
 }
--- domain/src/main/java/com/s/domain/gateway/InPlayerAssetsRepository.kt
+++ domain/src/main/java/com/s/domain/gateway/InPlayerAssetsRepository.kt
@@ -9,10 +9,12 @@ import io.reactivex.Single
  * Created by victor on 1/5/19
  */
 interface InPlayerAssetsRepository {
-    
+
     fun getItemDetails(id: Int, merchantUUID: String): Single<ItemDetailsEntity>
-    
+
     fun getItemAccess(id: Int): Single<ItemAccessEntity>
-    
+
+    fun getExternalAsset(assetType: String, externalId: String): Single<ItemDetailsEntity>
+
     fun getAccessFees(id: Int): Single<List<AccessFeeEntity>>
 }
