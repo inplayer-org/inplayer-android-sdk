bc20a67b1ab1e649e14fd9e700fe9cd448bf4056
Viktor <viktor.petrovski@adeva.co>
2019-10-16 22:12:06 +0200

    Fixed mapping models in ItemAccessModel

--- Data/src/main/java/com/sdk/data/model/asset/ItemAccessModel.kt
+++ Data/src/main/java/com/sdk/data/model/asset/ItemAccessModel.kt
@@ -1,12 +1,13 @@
 package com.sdk.data.model.asset
 
+import com.google.gson.annotations.SerializedName
 
 data class ItemAccessModel(val id: Long,
-                           val accountId: Long,
-                           val customerId: Long,
-                           val customerUUID: String,
-                           val ipAddress: String,
-                           val countryCode: String,
-                           val createdAt: Long,
-                           val expiresAt: Long,
-                           val itemDetailsModel: ItemDetailsModel)
+                           @SerializedName("account_id") val accountId: Long,
+                           @SerializedName("customer_id") val customerId: Long,
+                           @SerializedName("customer_uuid") val customerUUID: String,
+                           @SerializedName("ip_address") val ipAddress: String,
+                           @SerializedName("country_code") val countryCode: String,
+                           @SerializedName("created_at") val createdAt: Long,
+                           @SerializedName("expires_at") val expiresAt: Long,
+                           @SerializedName("item") val itemDetailsModel: ItemDetailsModel)
