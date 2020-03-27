c6484d67c42c76c4ebde9f8c039da62d2ffe03c0
Viktor <viktor.petrovski@adeva.co>
2020-03-27 09:30:50 +0100

    Add SerializedName to all properties in InPlayerAuthorizationModel

--- Data/src/main/java/com/sdk/data/model/account/InPlayerAuthorizationModel.kt
+++ Data/src/main/java/com/sdk/data/model/account/InPlayerAuthorizationModel.kt
@@ -2,10 +2,9 @@ package com.sdk.data.model.account
 
 import com.google.gson.annotations.SerializedName
 
-
 data class InPlayerAuthorizationModel(
     @SerializedName("access_token") val accessToken: String,
     @SerializedName("refresh_token") val refreshToken: String,
-    val account: InPlayerAccount,
-    val expires: Long
+    @SerializedName("account") val account: InPlayerAccount,
+    @SerializedName("expires") val expires: Long
 )
