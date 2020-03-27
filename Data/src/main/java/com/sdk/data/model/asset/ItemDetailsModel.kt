c46e5b07974a1772e32868d8b407cec988e26246
Viktor <viktor.petrovski@adeva.co>
2020-03-27 09:38:32 +0100

    Update ItemDetailsModel for SerializedName property

--- Data/src/main/java/com/sdk/data/model/asset/ItemDetailsModel.kt
+++ Data/src/main/java/com/sdk/data/model/asset/ItemDetailsModel.kt
@@ -5,17 +5,17 @@ import com.google.gson.annotations.SerializedName
 
 
 data class ItemDetailsModel(
-    val id: Long,
+    @SerializedName("id") val id: Long,
     @SerializedName("merchant_id") val merchantId: Long,
     @SerializedName("merchant_uuid") val merchantUUID: String,
     @SerializedName("is_active") val isActive: Boolean,
-    val title: String,
+    @SerializedName("title") val title: String,
     @SerializedName("created_at") val createdAt: Long,
     @SerializedName("updated_at") val updatedAt: Long,
     @SerializedName("access_fees") val accessFees: List<AccessFeeModel>,
     @SerializedName("access_control_type") val accessControlTypeModel: AccessControlTypeModel,
     @SerializedName("item_type") val itemTypeModel: ItemTypeModel,
-    val content: String?,
-    val metahash: Map<String, String>,
-    val metadata: List<ItemMetadataModel>
+    @SerializedName("content") val content: String?,
+    @SerializedName("metahash") val metahash: Map<String, String>,
+    @SerializedName("metadata") val metadata: List<ItemMetadataModel>
 )
