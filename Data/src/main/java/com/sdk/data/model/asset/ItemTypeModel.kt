b204cb0faf7242cd29e699c1cb470cfe780f7b40
Viktor <viktor.petrovski@adeva.co>
2020-03-27 09:38:44 +0100

    Update ItemTypeModel for serialized property

--- Data/src/main/java/com/sdk/data/model/asset/ItemTypeModel.kt
+++ Data/src/main/java/com/sdk/data/model/asset/ItemTypeModel.kt
@@ -3,4 +3,10 @@ package com.sdk.data.model.asset
 import com.google.gson.annotations.SerializedName
 
 
-data class ItemTypeModel(val id: Long, val name: String, @SerializedName("content_type") val contentType: String, val host: String, val description: String)
+data class ItemTypeModel(
+    @SerializedName("id") val id: Long,
+    @SerializedName("name") val name: String,
+    @SerializedName("content_type") val contentType: String,
+    @SerializedName("host") val host: String,
+    @SerializedName("description") val description: String
+)
