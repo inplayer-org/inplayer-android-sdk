751ef65648c4aff7a9c79f4ec36a8cf5f6429bb8
Viktor <viktor.petrovski@adeva.co>
2020-03-27 09:30:20 +0100

    Add serializedName on all properties in InPlayerAccount

--- Data/src/main/java/com/sdk/data/model/account/InPlayerAccount.kt
+++ Data/src/main/java/com/sdk/data/model/account/InPlayerAccount.kt
@@ -2,18 +2,18 @@ package com.sdk.data.model.account
 
 import com.google.gson.annotations.SerializedName
 
-
-data class InPlayerAccount(val id: Long,
-                           val email: String,
+data class InPlayerAccount(@SerializedName("id") val id: Long,
+                           @SerializedName("email")  val email: String,
                            @SerializedName("full_name") val fullName: String,
-                           val referrer: String,
-                           val roles: List<String>,
+                           @SerializedName("referrer") val referrer: String,
+                           @SerializedName("roles") val roles: List<String>,
                            @SerializedName("completed") val isCompleted: Boolean,
                            @SerializedName("created_at") val createdAt: Long,
                            @SerializedName("updated_at") val updatedAt: Long,
                            @SerializedName("merchant_id") val merchantId: String,
                            @SerializedName("merchant_uuid") val merchantUUID: String,
-                           val metadata: HashMap<String, String>,
-                           val uuid: String,
-                           val username: String
+                           @SerializedName("metadata") val metadata: HashMap<String, String>,
+                           @SerializedName("uuid") val uuid: String,
+                           @SerializedName("username") val username: String
 )
+
