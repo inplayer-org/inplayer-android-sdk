d4598286b0887a71a809c104086e9d4d2d5edcea
Viktor <viktor.petrovski@adeva.co>
2020-05-12 17:55:04 +0200

    Implement InPlayer models

--- InPlayer/src/main/java/com/sdk/inplayer/mapper/assets/MapAccessFee.kt
+++ InPlayer/src/main/java/com/sdk/inplayer/mapper/assets/MapAccessFee.kt
@@ -3,28 +3,39 @@ package com.sdk.inplayer.mapper.assets
 import com.sdk.domain.entity.asset.AccessFeeEntity
 import com.sdk.domain.entity.mapper.DomainMapper
 import com.sdk.inplayer.model.assets.InPlayerAccessFee
+import com.sdk.inplayer.model.assets.InPlayerExternalFees
+import com.sdk.inplayer.model.assets.InPlayerGeoRestriction
+import com.sdk.inplayer.model.assets.InPlayerSeasonalFee
 
 
-internal class MapAccessFee constructor(private val mapAccessType: MapAccessType, private val mapItemType: MapItemType,
-                               private val mapTrialPeriod: MapTrialPeriod, private val mapSetupFee: MapSetupFee)
-    : DomainMapper<AccessFeeEntity, InPlayerAccessFee> {
+internal class MapAccessFee constructor(
+    private val mapAccessType: MapAccessType, private val mapItemType: MapItemType,
+    private val mapTrialPeriod: MapTrialPeriod, private val mapSetupFee: MapSetupFee
+) : DomainMapper<AccessFeeEntity, InPlayerAccessFee> {
     
     override fun mapFromDomain(domainEntity: AccessFeeEntity): InPlayerAccessFee {
-        return InPlayerAccessFee(id = domainEntity.id,
-                merchantId = domainEntity.merchantId,
-                amount = domainEntity.amount,
-                currency = domainEntity.currency,
-                description = domainEntity.description,
-                accessType = mapAccessType.mapFromDomain(domainEntity.accessTypeEntity),
-                itemType = domainEntity.itemType,
-                trialPeriod = domainEntity.trialPeriodEntity?.let { mapTrialPeriod.mapFromDomain(it) },
-                setupFee = domainEntity.setupFeeEntity?.let { mapSetupFee.mapFromDomain(it) },
-                expiresAt = domainEntity.expiresAt)
+        return InPlayerAccessFee(
+            id = domainEntity.id,
+            merchantId = domainEntity.merchantId,
+            amount = domainEntity.amount,
+            currency = domainEntity.currency,
+            description = domainEntity.description,
+            accessType = domainEntity.accessTypeEntity?.let { mapAccessType.mapFromDomain(it) },
+            itemType = domainEntity.itemType,
+            trialPeriod = domainEntity.trialPeriodEntity?.let { mapTrialPeriod.mapFromDomain(it) },
+            setupFee = domainEntity.setupFeeEntity?.let { mapSetupFee.mapFromDomain(it) },
+            expiresAt = domainEntity.expiresAt,
+            startsAt = domainEntity.startsAt,
+            createdAt = domainEntity.createdAt,
+            updatedAt = domainEntity.updatedAt,
+            geoRestriction = domainEntity.geoRestrictionEntity?.let { InPlayerGeoRestriction(it) },
+            seasonalFee = domainEntity.seasonalFeeEntity?.let { InPlayerSeasonalFee(it) },
+            externalFees = domainEntity.externalFeesEntity?.let { InPlayerExternalFees(it) },
+            item = null
+        )
     }
     
     override fun mapToDomain(viewModel: InPlayerAccessFee): AccessFeeEntity {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-    
-    
 }
--- InPlayer/src/main/java/com/sdk/inplayer/model/assets/InPlayerAccessFee.kt
+++ InPlayer/src/main/java/com/sdk/inplayer/model/assets/InPlayerAccessFee.kt
@@ -1,13 +1,23 @@
 package com.sdk.inplayer.model.assets
 
+import com.sdk.domain.entity.asset.*
 
-class InPlayerAccessFee(val id: Long,
-                             val merchantId: Long,
-                             val amount: Float,
-                             val currency: String,
-                             val description: String,
-                             val accessType: InPlayerAccessType,
-                             val itemType: String,
-                             val trialPeriod: InPlayerTrialPeriod?,
-                             val setupFee: InPlayerSetupFee?,
-                             val expiresAt: Long?)
+class InPlayerAccessFee(
+    val id: Long,
+    val merchantId: Long,
+    val amount: Float,
+    val currency: String,
+    val description: String,
+    val accessType: InPlayerAccessType?,
+    val itemType: String,
+    val trialPeriod: InPlayerTrialPeriod?,
+    val setupFee: InPlayerSetupFee?,
+    val expiresAt: Long?,
+    val createdAt: Long?,
+    val updatedAt: Long?,
+    val startsAt: Long?,
+    val item: InPlayerItem?,
+    val geoRestriction: InPlayerGeoRestriction?,
+    val externalFees: InPlayerExternalFees?,
+    val seasonalFee: InPlayerSeasonalFee?
+)
