5b2bcbe698eec7b1e538494189085066f8dedd70
Viktor <viktor.petrovski@adeva.co>
2020-05-31 23:36:29 +0200

    Change start_date & end_date to be Long instead of String

--- Data/src/main/java/com/sdk/data/model/asset/v2/VoucherApiModel.kt
+++ Data/src/main/java/com/sdk/data/model/asset/v2/VoucherApiModel.kt
@@ -7,11 +7,11 @@ data class VoucherApiModel(
     val discount: Int,
     val discount_duration: Int,
     val discount_period: String?,
-    val end_date: String?,
+    val end_date: Long,
     val id: Int,
     val name: String?,
     val rebill_discount: Int,
-    val start_date: String?,
+    val start_date: Long,
     val usage_counter: Int,
     val usage_limit: Int
 ) {
