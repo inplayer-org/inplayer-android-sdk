1cfc90f306eef7fc36fd9be8ba8572820a0c8b5c
krstem <krste.moskov@sourcico.com>
2022-03-04 15:08:51 +0100

    fix issue

--- InPlayer/src/main/java/com/sdk/inplayer/mapper/payment/InPlayerExternalPaymentFailedMapper.kt
+++ InPlayer/src/main/java/com/sdk/inplayer/mapper/payment/InPlayerExternalPaymentFailedMapper.kt
@@ -3,7 +3,7 @@ package com.sdk.inplayer.mapper.payment
 import com.sdk.inplayer.model.notification.InPlayerNotification
 import com.sdk.notification.model.legacy.InPlayerExternalPaymentFailedNotificationResource
 
-class InPlayerExternalPaymentFailedMapper(
+data class InPlayerExternalPaymentFailedMapper(
     val resource: InPlayerExternalPaymentFailedNotificationResource,
     override val type: String, override val timestamp: Long
 ) : InPlayerNotification
